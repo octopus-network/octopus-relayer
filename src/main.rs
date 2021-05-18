be33ed3ff13d035c715fadc1e1357dd68fe07dd5
Yuanchao Sun <yuanchao.sun@gmail.com>
2021-05-18 22:04:37 +0800

    Test all interfaces related to beefy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use beefy_light_client::{validator_set, Commitment, Payload, SignedCommitment};
+use beefy_primitives::ecdsa::AuthoritySignature as BeefySignature;
+use codec::Decode;
+use substrate_subxt::{system::System, BlockNumber, ClientBuilder};
+
+mod beefy;
+mod runtime;
+use beefy::{AuthoritiesStoreExt, ValidatorSetIdStoreExt};
 
 fn main() {
     let mut lc = beefy_light_client::new();
@@ -16,5 +23,36 @@ fn main() {
         1.into(),
         (),
     ));
+    println!("verify result: {:?}", result);
+    let result = async_std::task::block_on(run());
     println!("result: {:?}", result);
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let client = ClientBuilder::<runtime::AppchainRuntime>::new()
+        .set_url("ws://127.0.0.1:9944")
+        .build()
+        .await?;
+    let mut commitments = client.subscribe_justifications().await?;
+    while let Some(commitment) = commitments.next().await {
+        let decoded: beefy_primitives::SignedCommitment<
+            <runtime::AppchainRuntime as System>::BlockNumber,
+            beefy_primitives::MmrRootHash,
+            BeefySignature,
+        > = Decode::decode(&mut &*commitment.0).unwrap();
+        println!("commitment: {:?}", decoded);
+        let set_id = client.validator_set_id(None).await.unwrap();
+        println!("set_id: {:?}", set_id);
+        let authorities = client.authorities(None).await.unwrap();
+        println!("authorities: {:?}", authorities);
+        let hash = client
+            .block_hash(Some(BlockNumber::from(decoded.commitment.block_number)))
+            .await
+            .unwrap();
+        let proof = client
+            .generate_proof(decoded.commitment.block_number.into(), Some(hash.unwrap()))
+            .await;
+        println!("proof: {:?}", proof);
+    }
+    Ok(())
 }
---
+++ src/runtime.rs
@@ -0,0 +1,67 @@
+use crate::beefy::Beefy;
+use beefy_primitives::ecdsa::AuthorityId as BeefyId;
+use sp_runtime::{
+    generic::Header,
+    traits::{BlakeTwo256, IdentifyAccount, Verify},
+    MultiSignature, OpaqueExtrinsic,
+};
+use substrate_subxt::{
+    balances::{AccountData, Balances, BalancesEventTypeRegistry},
+    contracts::{Contracts, ContractsEventTypeRegistry},
+    extrinsic::DefaultExtra,
+    register_default_type_sizes,
+    session::{Session, SessionEventTypeRegistry},
+    staking::{Staking, StakingEventTypeRegistry},
+    sudo::{Sudo, SudoEventTypeRegistry},
+    system::{System, SystemEventTypeRegistry},
+    BasicSessionKeys, EventTypeRegistry, Runtime,
+};
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct AppchainRuntime;
+
+impl Staking for AppchainRuntime {}
+
+impl Runtime for AppchainRuntime {
+    type Signature = MultiSignature;
+    type Extra = DefaultExtra<Self>;
+
+    fn register_type_sizes(event_type_registry: &mut EventTypeRegistry<Self>) {
+        event_type_registry.with_system();
+        event_type_registry.with_balances();
+        event_type_registry.with_session();
+        event_type_registry.with_staking();
+        event_type_registry.with_contracts();
+        event_type_registry.with_sudo();
+        register_default_type_sizes(event_type_registry);
+    }
+}
+
+impl System for AppchainRuntime {
+    type Index = u32;
+    type BlockNumber = u32;
+    type Hash = sp_core::H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
+    type Address = sp_runtime::MultiAddress<Self::AccountId, u32>;
+    type Header = Header<Self::BlockNumber, BlakeTwo256>;
+    type Extrinsic = OpaqueExtrinsic;
+    type AccountData = AccountData<<Self as Balances>::Balance>;
+}
+
+impl Balances for AppchainRuntime {
+    type Balance = u128;
+}
+
+impl Session for AppchainRuntime {
+    type ValidatorId = <Self as System>::AccountId;
+    type Keys = BasicSessionKeys;
+}
+
+impl Contracts for AppchainRuntime {}
+
+impl Sudo for AppchainRuntime {}
+
+impl Beefy for AppchainRuntime {
+    type AuthorityId = BeefyId;
+}
