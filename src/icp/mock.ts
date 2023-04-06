8b5e1a2775c761317d95da862be6571a6d0cda24
anonymousGiga <cryptonymGong@gmail.com>
2023-04-06 18:57:42 +0800

    update

--- src/icp/mock.ts
+++ src/icp/mock.ts
@@ -378,8 +378,8 @@ export const mmr_proof = [
 export const initial_public_keys = [
   "0x020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a1", // Alice
   "0x0390084fdbf27d2b79d26a4f13f0ccd982cb755a661969143c37cbc49ef5b91f27", // Bob
-  // "0x0389411795514af1627765eceffcbd002719f031604fadd7d188e2dc585b4e1afb", // Charlie
-  // "0x03bc9d0ca094bd5b8b3225d7651eac5d18c1c04bf8ae8f8b263eebca4e1410ed0c", // Dave
+  "0x0389411795514af1627765eceffcbd002719f031604fadd7d188e2dc585b4e1afb", // Charlie
+  "0x03bc9d0ca094bd5b8b3225d7651eac5d18c1c04bf8ae8f8b263eebca4e1410ed0c", // Dave
   // "0x031d10105e323c4afce225208f71a6441ee327a65b9e646e772500c74d31f669aa", // Eve
 ];
 
--- src/near-sub.ts
+++ src/near-sub.ts
@@ -6,7 +6,10 @@ const path = require("path");
 const homedir = require("os").homedir();
 
 const CREDENTIALS_DIR = ".near-credentials";
-const CONTRACT_NAME = "barnacle-latest.registry.test_oct.testnet";
+// const CONTRACT_NAME = "barnacle-latest.registry.test_oct.testnet";
+const CONTRACT_NAME = "andy-test-relayer.registry.test_oct.testnet";
+// const ACCOUNT_ID = "test-relayer.testnet";
+const ACCOUNT_ID = "oct-pallet-test.testnet";
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
 const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 const DEFAULT_FUNCTION_CALL_GAS = new BN("300000000000000");
@@ -14,44 +17,61 @@ const DEFAULT_FUNCTION_CALL_GAS = new BN("300000000000000");
 const config = {
   keyStore,
   networkId: "testnet",
-  nodeUrl: "https://rpc.testnet.near.org",
+  // nodeUrl: "https://rpc.testnet.near.org",
+  nodeUrl: "https://near-testnet.infura.io/v3/dabe9e95376540b083ae09909ea7c576",
 };
 
 const projectId = "octopus-dev-309403";
 const topicVersionedFinalityProof =
   "projects/octopus-dev-309403/topics/test-appchain-versioned-finality-proof";
 const topicMessage = "projects/octopus-dev-309403/topics/test-appchain-message";
+const topicSignedMessage =
+  "projects/octopus-dev-309403/topics/test-appchain-signed-message";
 const topicUnsignedMessage =
   "projects/octopus-dev-309403/topics/test-appchain-unsigned-message";
 const subscriptionMessage =
   "projects/octopus-dev-309403/subscriptions/test-appchain-message-sub";
+const subscriptionSignedMessage =
+  "projects/octopus-dev-309403/subscriptions/test-appchain-signed-message-sub";
 const subscriptionUnsignedMessage =
   "projects/octopus-dev-309403/subscriptions/test-appchain-unsigned-message-sub";
 
-export interface MessageWithSignature {
+interface MessageWithSignature {
   encoded_messages: number[];
   verification_proxy_signature: number[] | null;
 }
 
 async function main() {
-  await handleMessage();
+  const isWitnessMode = await checkAnchorIsWitnessMode();
+  if (isWitnessMode) {
+    await handleMessage();
+  } else {
+    await handleVersionedFinalityProof();
+  }
 }
 
-function handleVersionedFinalityProof() {}
+async function handleVersionedFinalityProof() {
+  await synchronousPull(
+    parseSignedMessage,
+    projectId,
+    subscriptionSignedMessage
+  );
+}
 
 async function handleMessage() {
-  await synchronousPull(projectId, subscriptionMessage);
+  await synchronousPull(parseUnsignedMessage, projectId, subscriptionMessage);
 }
 
 // Creates a client; cache this for further use.
 const subClient = new v1.SubscriberClient();
 
 export async function synchronousPull(
+  parseFunction: (text: String) => MessageWithSignature,
   projectId: string,
   subscriptionNameOrId: string
 ) {
   const near = await connect({ ...config, keyStore });
-  const account = await near.account("test-relayer.testnet");
+  const account = await near.account(ACCOUNT_ID);
   // The low level API client requires a name only.
   const formattedSubscription =
     subscriptionNameOrId.indexOf("/") >= 0
@@ -72,17 +92,14 @@ export async function synchronousPull(
   const ackIds: string[] = [];
   for (const message of response.receivedMessages ?? []) {
     console.log(`Received message: ${message.message?.data}`);
-    const obj = JSON.parse(`${message.message?.data}`);
+
+    let args = parseFunction(`${message.message?.data}`);
+    console.log(`args: ${args}`);
 
     const result = await account.functionCall({
       contractId: CONTRACT_NAME,
       methodName: "stage_and_apply_appchain_messages",
-      args: {
-        encoded_messages: Array.from(
-          Buffer.from(obj.crossChainMessages.slice(2), "hex")
-        ),
-        verification_proxy_signature: null,
-      },
+      args: args,
       gas: DEFAULT_FUNCTION_CALL_GAS,
       attachedDeposit: new BN("0"),
     });
@@ -105,6 +122,54 @@ export async function synchronousPull(
   }
 
   console.log("Done.");
+}
+
+async function checkAnchorIsWitnessMode(): Promise<Boolean> {
+  try {
+    const near = await connect({ ...config, keyStore });
+    const account = await near.account(ACCOUNT_ID);
+    const anchorSettings = await account.viewFunction(
+      CONTRACT_NAME,
+      "get_anchor_settings",
+      {}
+    );
+
+    // const contract = await near.Contract(account, CONTRACT_NAME, {
+    //   viewMethods: ["get_anchor_settings"],
+    //   changeMethods: [],
+    //   sender: account.accountId,
+    // });
+
+    console.log("setting: ", anchorSettings);
+    return anchorSettings ? anchorSettings.witness_mode : false;
+  } catch (error) {
+    console.error("checkAnchorIsWitnessMode error", error);
+    return false;
+  }
+}
+
+function parseSignedMessage(text: any): MessageWithSignature {
+  const obj = JSON.parse(text);
+  let messageWithSignature: MessageWithSignature = {
+    //TODO: Need check
+    encoded_messages: Array.from(Buffer.from(obj.encoded_messages, "hex")),
+    verification_proxy_signature: Array.from(
+      Buffer.from(obj.verification_proxy_signature, "hex")
+    ),
+  };
+
+  return messageWithSignature;
+}
+
+function parseUnsignedMessage(text: any): MessageWithSignature {
+  const obj = JSON.parse(text);
+  let messageWithSignature: MessageWithSignature = {
+    encoded_messages: Array.from(
+      Buffer.from(obj.crossChainMessages.slice(2), "hex")
+    ),
+    verification_proxy_signature: null,
+  };
+  return messageWithSignature;
 }
 
 main().catch(console.error);
